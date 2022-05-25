41031ded33c46cd47ba3ffce5a2928961b7e0c37
Xiao <xmeng1@ualberta.ca>
2022-05-24 21:57:24 -0600

    add createGameboard factory function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const Ship = (length) => {
+const createShip = (length) => {
   const shipBody = new Array(length).fill(null);
   const hit = (index) => {
     if (index >= 0 || index <= length - 1) {
@@ -12,8 +12,84 @@ const Ship = (length) => {
       return false;
     }
   };
+  const getShipBody = () => {
+    return [...shipBody];
+  }
 
-  return { hit, isSunk };
+  return { hit, isSunk, getShipBody };
 };
 
-export { Ship }
+const createGameboard = () => {
+  const ships = [];
+  const missedShots = [];
+
+  const addShip = (name, length, orientation, startPosition) => {
+    const newShip = createShip(length);
+    let [rowIndex, colIndex] = startPosition;
+
+    newShip.name = name;
+    newShip.coordinates = [];
+
+    if (orientation === 'horizontal') {
+      for (let i = 0; i < length; i++) {
+        newShip.coordinates.push([rowIndex, i + colIndex]);
+      }
+    } else if (orientation === 'vertical') {
+      for (let i = 0; i < length; i++) {
+        newShip.coordinates.push([i + rowIndex, colIndex]);
+      }
+    }
+
+    ships.push(newShip);
+  };
+
+  const getShip = (shipName) => {
+    const ship = ships.find((ship) => ship.name === shipName);
+
+    return {...ship};
+  };
+
+  const receiveAttack = (attackCoordinate) => {
+    let hit = false;
+    let hitShip;
+    let hitIndex;
+    
+    for (const ship of ships) {
+      hitIndex = ship.coordinates.findIndex((coordinate) => {
+        if (coordinate[0] === attackCoordinate[0] &&
+          coordinate[1] === attackCoordinate[1]) {
+            hit = true;
+            hitShip = ship;
+
+            return true;
+        }
+      });
+
+      if (hitShip) {
+        break;
+      }
+    }
+
+    if (hit) {
+      hitShip.hit(hitIndex);
+    } else {
+      missedShots.push(attackCoordinate);
+    }    
+  };
+
+  const areAllShipsSunk = () => {
+    const allShipsSunk = ships.every((ship) => {
+      if (ship.getShipBody().every((x) => x === 'hit')) {
+        return true;
+      }
+      
+      return false;
+    });
+
+    return allShipsSunk;
+  };
+
+  return { addShip, getShip, receiveAttack, areAllShipsSunk };
+};
+
+export { createShip, createGameboard }
--- app.test.js
+++ app.test.js
@@ -1,22 +1,56 @@
-import { Ship } from './app.js';
+import { createShip, createGameboard } from './app.js';
 
-describe('Ship factory function tests', () => {
-  test('Ship returns an object', () => {
-    expect(typeof(Ship())).toBe('object');
+describe('createShip factory function tests', () => {
+  test('createShip returns an object', () => {
+    expect(typeof(createShip())).toBe('object');
   });
   
   test('ship is not sunk if part of its body has hits', () => {
-    const ship = Ship(3);
+    const ship = createShip(3);
     ship.hit(0);
     ship.hit(1);
     expect(ship.isSunk()).toBe(false);
   });
   
   test('ship is sunk if its entire body has hits', () => {
-    const ship = Ship(3);
+    const ship = createShip(3);
     ship.hit(0);
     ship.hit(1);
     ship.hit(2);
     expect(ship.isSunk()).toBe(true);
+  });
+});
+
+describe('createGameboard factory function tests', () => {
+  test('createGameboard returns an object', () => {
+    expect(typeof(createGameboard())).toBe('object');
+  });
+
+  test('gameboard can place ships', () => {
+    const gameboard = createGameboard();
+    gameboard.addShip('Carrier', 5, 'vertical', [0, 0]);
+    expect(gameboard.getShip('Carrier').coordinates).toStrictEqual(
+      [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]]);
+  });
+
+  test('gameboard can recieve attacks and hit ship', () => {
+    const gameboard = createGameboard();
+    gameboard.addShip('Carrier', 5, 'vertical', [0, 0]);
+    gameboard.receiveAttack([0, 0]);
+    expect(gameboard.getShip('Carrier').getShipBody()[0]).toBe('hit');
+  });
+
+  test('gameboard can check if all ships have sunk', () => {
+    const gameboard = createGameboard();
+    gameboard.addShip('Carrier', 2, 'vertical', [0, 0]);
+    gameboard.addShip('Destroyer', 2, 'horizontal', [7, 0]);
+    gameboard.receiveAttack([0, 0]);
+    gameboard.receiveAttack([1, 0]);
+    gameboard.receiveAttack([2, 0]);
+    gameboard.receiveAttack([3, 0]);
+    gameboard.receiveAttack([4, 0]);
+    gameboard.receiveAttack([7, 0]);
+    gameboard.receiveAttack([7, 1]);
+    expect(gameboard.areAllShipsSunk()).toBe(true);
   });
 });
